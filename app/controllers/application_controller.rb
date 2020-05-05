c22a6719700736e63fae60874f866a8c0e7617e1
Joe Snyder <snyderjosephd@yahoo.com>
2020-05-04 20:14:11 -0500

    Added email and password form and render it when user visits /users/new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+
+    def current_user
+        if session[:session_token].nil?
+            return nil
+        end
+
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_no_user!
+        redirect_to '/' if current_user
+    end
+
+    def require_current_user
+        if current_user.nil?
+            redirect_to '/'
+        end
+    end
 end
